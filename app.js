d65db6e524b42fc3f869aebd99c18f210b5d9f29
Kenrick Beckett <kenrick.beckett@gmail.com>
2012-04-03 00:03:56 -0500

    Update reference guide to get all products

--- app.js
+++ app.js
@@ -32,46 +32,72 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-shop = "hand-ratke-and-green2603";
-token = '';
+shop_name = "hand-ratke-and-green2603";
+token_id = '';
 
 // Routes
 app.get('/', function(req, res) {
-	if(req.session.shopify)
+	var shop = undefined, token = undefined;
+
+	if(req.session.shopify){
 		token = req.session.shopify.t
-	session = nodify.createSession(shop, token, config.apiKey, config.secret);
+		shop = req.session.shopify.shop
+	}
+
+	if(shop !== undefined && token !== undefined) {
+		session = nodify.createSession(shop, token, config.apiKey, config.secret);
+
+		if(session.valid()){
+			product = {
+				  "product": {
+				    "body_html": "<strong>YAY!</strong>",	
+				  }   
+			};      
+			session.product.all(function(err, data){
+				if(err) { console.log(data); throw err;}
+				res.send(data);
+			});
+		}
+	}
+	else {
+		res.redirect('/login');
+	}
+		
+});
+
+
+app.get('/login', function(req, res) {
+
+	session = nodify.createSession(shop_name, token_id, config.apiKey, config.secret);
 	if(session.valid())
 	{
-		if(token === '') {
+		if(token_id === '') {
 			res.redirect(session.createPermissionUrl());
 		}
 		else{
-			res.send("WE ARE IN!!");
+			res.redirect('/');
 		}
 	}
 	else{
-		res.send("nope");
+		res.send("Could not log in");
 	}
 
 });
-
-app.get('/login', function(req, res) {
-	
-});
-
 
 
 app.get('/login/finalize', function(req, res) {
 	params = req.query;
 	req.session.shopify = params;
 	session = nodify.createSession(req.query.shop, params.t, config.apiKey, config.secret, params);
-	console.log(session.site());
-	res.redirect("/");	
+	if(session.valid()){
+		res.redirect("/");
+	}
+	else {
+		res.send("Could not finalize");
+	}
+	
 	
 });
-
-
-
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
