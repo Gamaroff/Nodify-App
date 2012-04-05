9adb7e51072cd4aa6a21afb6764d905bc34780c9
Kenrick Beckett <kenrick.beckett@gmail.com>
2012-04-05 17:50:54 -0500

    added Procfile

--- app.js
+++ app.js
@@ -132,5 +132,10 @@ app.get('/logout', function(req, res) {
 	res.redirect('/');
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+
+	console.log("Running on: ", app.address().port);
+});
+
